caec20e5bd3b31eb29ffa64008a2bb3a8bf2f7d6
thiago rossato <thiago.rossato.tr@gmail.com>
2020-04-21 18:47:59 -0300

    criação do projeto e add as dependencias css file

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import profile from './assets/IMG_4979.JPG'
+
+function App (){
+  return <img width= "500" src={profile}/>
+}
+
+export default App;
