f9198e6c6952a91a314dcd596be358650deaef46
thiago rossato <thiago.rossato.tr@gmail.com>
2020-04-22 12:05:58 -0300

    reiniciando o projeto

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import profile from './assets/IMG_4979.JPG'
-
-function App (){
-  return <img width= "500" src={profile}/>
-}
-
-export default App;
